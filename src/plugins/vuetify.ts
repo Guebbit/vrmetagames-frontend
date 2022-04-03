3a8e09ab27e37c5656f06a4d5cedce8a4429ac64
guerzoni_andrea <guerzoni.andrea91@gmail.com>
2022-04-04 01:33:49 +0200

    theming

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -5,5 +5,25 @@ import "vuetify/styles";
 // Vuetify
 import { createVuetify } from "vuetify";
 
-export default createVuetify();
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
+export default createVuetify({
+    theme: {
+        defaultTheme: "default",
+        themes: {
+            default: {
+                dark: true,
+                colors: {
+                    background: "#212121",
+                    surface: "#212121",
+                    primary: "#0086ff",
+                    secondary: "#ff7b00",
+                    error: "#B00020",
+                    info: "#2196F3",
+                    success: "#4CAF50",
+                    warning: "#FB8C00",
+                },
+                variables: {}, // BUGFIX: ADD AN EMPTY OBJECT
+            },
+        },
+    },
+});
